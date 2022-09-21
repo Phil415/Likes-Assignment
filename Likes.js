ccb8e9ad38a2f5367e9f14699b500ac7802cfd50
Phil415 <phillevy415@gmail.com>
2022-09-21 15:47:42 -0700

    first commit

---
+++ Likes.js
@@ -0,0 +1,13 @@
+/* var countElement= document.querySelector();
+*/
+
+console.log(countElement);
+
+function add1(button){
+    var parent = button.parentNode;
+    var pTag = parent.querySelector("p");
+    pTag.innerText++
+}
+
+
+
